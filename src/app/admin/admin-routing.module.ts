33accb1ea6d807553d80deec2cab89dfdfedf56f
NikunjItaliya <nikuj.italiya@ghanshyamdigital.com>
2023-06-14 20:03:37 +0530

    admin routing file

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PanelComponent} from "./panel/panel.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'orders',
+    component : PanelComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomeRoutingModule { }
+export class AdminRoutingModule { }
